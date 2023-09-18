74df1ab3492fb61bdb3c3a45147819b84a4458c7
Sanjay-312 <sanjeevabodagalla@gmail.com>
2023-09-18 16:58:44 +0530

    [sanjeeva] added data into queue

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace QueueAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            QueueExample obj = new QueueExample();
+            obj.enqueData(56);
+            obj.enqueData(30);
+            obj.enqueData(70);
+
+            obj.DisplayData();
+        }
+    }
+}
